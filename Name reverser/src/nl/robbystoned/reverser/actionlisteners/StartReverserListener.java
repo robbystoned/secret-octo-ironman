4b59dd6915dbd9761499f27458d84b0101d60642
robbystoned <robbystoned@gmail.com>
2014-03-13 22:27:01 +0100

    basic concept for single file mode now working

--- Name reverser/src/nl/robbystoned/reverser/actionlisteners/StartReverserListener.java
+++ Name reverser/src/nl/robbystoned/reverser/actionlisteners/StartReverserListener.java
@@ -3,7 +3,6 @@ package nl.robbystoned.reverser.actionlisteners;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +11,7 @@ import nl.robbystoned.reverser.ui.NameReverserUI;
 public class StartReverserListener implements ActionListener {
 	private NameReverserUI ui;
 	public StartReverserListener(NameReverserUI nameReverserUI) {
-		this.ui = nameReverserUI;
+		this.ui = nameReverserUI;//pass the ui to be able to get to the values in the interface
 	}
 
 	@Override
@@ -26,24 +25,39 @@ public class StartReverserListener implements ActionListener {
 				List<String> selectedExtensions = Arrays.asList(ui.getExtensions().getSelectedItems());
 				if(fileOrDir.isDirectory()){
 					//dir mode
-					
-					
 				} else {
 					//single file mode.
-					String fileName = fileOrDir.getName();
-					processSingleFile(selectedExtensions, fileName);
+					String fileName = fileOrDir.getAbsolutePath();
+					processSingleFile(selectedExtensions, fileOrDir);
 				}
 			}
 		}
-
 	}
 
-	private void processSingleFile(List<String> selectedExtensions,
-			String fileName) {
-		String extension = fileName.substring(fileName.lastIndexOf("."), fileName.length());
+	/**
+	 * reverses the name of one single file if the extension matches any of the extensions
+	 * in the arraylist
+	 * @param selectedExtensions
+	 * @param fileName
+	 * @param baseDir 
+	 */
+	private void processSingleFile(List<String> selectedExtensions, File file) {
+		
+		String fileName = file.getName();
+		String baseDir = file.getParent();
+		String extension = fileName .substring(fileName.lastIndexOf("."), fileName.length());
 		if(selectedExtensions.contains(extension)){
-			
+			String nameWithoutExtension = fileName.substring(0, fileName.lastIndexOf("."));
+			System.out.println(file.renameTo(new File(baseDir + "/" + reverse(nameWithoutExtension)+extension)));
 		}
 	}
-
+	
+	/**
+	 * reverses the given string
+	 * @param the string to revers
+	 * @return the reversed string
+	 */
+	private String reverse(String s) {
+	    return new StringBuffer(s).reverse().toString();
+	}
 }
